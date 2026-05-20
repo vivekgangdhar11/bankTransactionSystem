2e4e9fea3741ec9941fd19578291be99c800f03f
Vivek Gangdhar <vivekgangdhar11@gmail.com>
2026-05-20 11:24:07 +0530

    email send facility added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel=require('../models/user.model');
 const jwt=require('jsonwebtoken');
+const emailService=require('../services/email.service');
 
 async function userRegisterController(req,res){
   const {name,email,password}=req.body;
@@ -22,6 +23,8 @@ async function userRegisterController(req,res){
     },
     token
   });
+
+  await emailService.sendRegistrationEmail(user.email,user.name);
 }
 
 async function userLoginController(req,res){
