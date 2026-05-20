1ff0b1f1c5da219006e25cef65d19e3796a17395
Vivek Gangdhar <vivekgangdhar11@gmail.com>
2026-05-20 11:47:13 +0530

    account model is created for transaction

---
+++ src/models/account.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const accountSchema = new mongoose.Schema({
+  user: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: [true,"Account must be associate to user"],
+    index:true
+  },
+  status: {
+    enum:{
+      values:["active","frozen","closed"],
+      message:"Status must be either active , closed or frozen"
+    }
+  },
+  currency: {
+    type: String,
+    required: [true,"Currency is required for account"],
+    default:"INR"
+  }
+    
+},{
+  timestamps:true
+})
+
+const accountModel = mongoose.model('account', accountSchema);
+
+accountSchema.index({ user: 1 , status: 1 }); // Ensure one account per user
+
+module.exports = accountModel;
