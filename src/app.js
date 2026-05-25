2af48d7b9332122e5f44ed0b7a11ee1edb869407
Vivek Gangdhar <vivekgangdhar11@gmail.com>
2026-05-25 12:02:59 +0530

    transaction routes created

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express=require('express');
 
 const authRouter=require('./routes/auth.routes');
 const accountRouter=require('./routes/account.routes');
+const transactionRoutes=require('./routes/transaction.routes');
 
 const cookieParser=require('cookie-parser');
 
@@ -17,5 +18,6 @@ app.get('/',(req,res)=>{
 
 app.use('/api/auth',authRouter);
 app.use('/api/accounts',accountRouter);
+app.use('/api/transactions',transactionRoutes);
 
 module.exports=app;
---
+++ src/routes/transaction.routes.js
@@ -0,0 +1,8 @@
+const {Router} = require('express');
+
+const transactionRoutes = Router();
+
+
+
+
+module.exports = transactionRoutes;
